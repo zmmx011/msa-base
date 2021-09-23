72ebb31648cf1cd70684c9002e48dafd7e807bac
damu <damu@inveniacorp.com>
2021-09-23 18:59:57 +0900

    space 빌드, 이미지 생성, 이미지 푸쉬 테스트

--- .space.kts
+++ .space.kts
@@ -5,5 +5,27 @@
 */
 
 job("Build") {
-    gradlew("openjdk:17", "build")
+    container(displayName = "Run gradle build", image = "openjdk") {
+        shellScript {
+            content = """
+                    ./gradlew build
+                    cp -r build $mountDir/share
+                """
+        }
+    }
+    docker {
+        resources {
+            cpu = 1.cpu
+            memory = 2000.mb
+        }
+        beforeBuildScript {
+            content = "cp -r  $mountDir/share docker"
+        }
+        build {
+            context = "docker"
+        }
+        push("invenia.registry.jetbrains.space/p/sso/containers/config-server") {
+            tags("0.\$JB_SPACE_EXECUTION_NUMBER", "lts")
+        }
+    }
 }
