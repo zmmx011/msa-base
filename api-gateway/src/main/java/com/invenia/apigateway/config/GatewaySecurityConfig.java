66712aae8c2c9c1331f1ea7069b16f821279c976
damu <damu@inveniacorp.com>
2022-04-11 17:42:05 +0900

    Gateway 인증만 담당하도록 수정

--- api-gateway/src/main/java/com/invenia/apigateway/config/GatewaySecurityConfig.java
+++ api-gateway/src/main/java/com/invenia/apigateway/config/GatewaySecurityConfig.java
@@ -1,26 +1,21 @@
 package com.invenia.apigateway.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
 @EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
 public class GatewaySecurityConfig {
 
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-    http
-        .authorizeExchange()
-        .anyExchange()
-        .authenticated()
-        .and()
-        .oauth2Login() // to redirect to oauth2 login page.
-        .and()
-        .oauth2ResourceServer()
-        .jwt();
+    http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+        .oauth2Login(withDefaults());
     http.csrf().disable();
     return http.build();
   }
