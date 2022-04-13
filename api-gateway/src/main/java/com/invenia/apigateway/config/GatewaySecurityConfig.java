9bb6411c957d7cd34dfae2d04bf6f9ba26b11b53
damu <damu@inveniacorp.com>
2022-04-13 11:43:03 +0900

    OAuth2 Client -> Resource Server 변경

--- api-gateway/src/main/java/com/invenia/apigateway/config/GatewaySecurityConfig.java
+++ api-gateway/src/main/java/com/invenia/apigateway/config/GatewaySecurityConfig.java
@@ -1,7 +1,5 @@
 package com.invenia.apigateway.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -14,8 +12,9 @@ public class GatewaySecurityConfig {
 
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-    http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
-        .oauth2Login(withDefaults());
+    http.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+        .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
     http.csrf().disable();
     return http.build();
   }
--- api-gateway/src/main/java/com/invenia/apigateway/controller/GatewayController.java
+++ api-gateway/src/main/java/com/invenia/apigateway/controller/GatewayController.java
@@ -1,7 +1,5 @@
 package com.invenia.apigateway.controller;
 
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.WebSession;
@@ -9,10 +7,6 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class GatewayController {
-  @GetMapping(value = "/token")
-  public Mono<String> token(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
-    return Mono.just(authorizedClient.getAccessToken().getTokenValue());
-  }
 
   @GetMapping("/")
   public Mono<String> index(WebSession session) {
