6d658281692a72e6eeef0df7d505ca52b03b2705
damu <damu@inveniacorp.com>
2022-04-18 22:26:34 +0900

    cors

--- api-gateway/src/main/java/com/invenia/apigateway/config/GatewaySecurityConfig.java
+++ api-gateway/src/main/java/com/invenia/apigateway/config/GatewaySecurityConfig.java
@@ -1,10 +1,13 @@
 package com.invenia.apigateway.config;
 
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -12,10 +15,20 @@ public class GatewaySecurityConfig {
 
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-    http.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+    http.cors()
+        .and()
+        .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
         .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
 
-    http.csrf().disable();
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource(
+      GlobalCorsProperties globalCorsProperties) {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    globalCorsProperties.getCorsConfigurations()
+        .forEach(source::registerCorsConfiguration);
+    return source;
   }
 }
